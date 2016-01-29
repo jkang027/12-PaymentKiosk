3d0b72bf23c276560e4cb6a516812a3fcf7b5f79
Jun Kang <jkang027@yahoo.com>
2016-01-28 16:10:50 -0800

    added a small coffeshop type pos

---
+++ PaymentKiosk/PaymentKiosk.Core/Domain/StoreMenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentKiosk.Core.Domain
+{
+    public class StoreMenuItem
+    {
+        public string ItemName { get; set; }
+        public decimal ItemCost { get; set; }
+    }
+}
--- PaymentKiosk/PaymentKiosk/MainWindow.xaml.cs
+++ PaymentKiosk/PaymentKiosk/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using PaymentKiosk.Core.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,9 +23,77 @@ namespace PaymentKiosk
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static ObservableCollection<StoreMenuItem> checkoutList = new ObservableCollection<StoreMenuItem>();
+
+        private StoreMenuItem currentlySelectedItem = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            dataGrid.ItemsSource = checkoutList;
+        }
+
+        public static decimal totalCost;
+        private void getTotal()
+        {
+            totalCost = 0;
+            for (var i = 0; i < checkoutList.Count; i++)
+            {
+                totalCost = totalCost + checkoutList[i].ItemCost;
+            }
+            textBlockTotalCost.Text = "$" + totalCost;
+            textBoxAmount.Text = totalCost.ToString();
+        }
+
+        private void buttonCoffee_Click(object sender, RoutedEventArgs e)
+        {
+            StoreMenuItem newCoffee = new StoreMenuItem
+            {
+                ItemName = "Coffee",
+                ItemCost = 2.99M
+            };
+            checkoutList.Add(newCoffee);
+            getTotal();
+        }
+
+        private void buttonMuffin_Click(object sender, RoutedEventArgs e)
+        {
+            StoreMenuItem newMuffin = new StoreMenuItem
+            {
+                ItemName = "Muffin",
+                ItemCost = 4.99M
+            };
+            checkoutList.Add(newMuffin);
+            getTotal();
+        }
+
+        private void buttonCroissant_Click(object sender, RoutedEventArgs e)
+        {
+            StoreMenuItem newCroissant = new StoreMenuItem
+            {
+                ItemName = "Croissant",
+                ItemCost = 3.99M
+            };
+            checkoutList.Add(newCroissant);
+            getTotal();
+        }
+        
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            currentlySelectedItem = (StoreMenuItem)dataGrid.SelectedItem;
+        }
+
+        private void buttonDeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+            checkoutList.Remove(currentlySelectedItem);
+            getTotal();
+            textBlockTotalCost.Text = "$" + totalCost;
+            textBoxAmount.Text = totalCost.ToString();
+        }
+
+        private void buttonCheckout_Click(object sender, RoutedEventArgs e)
+        {
+            tabControl.SelectedIndex = 1;
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
